b01d9916ed6ec63a480e10e1dcb3572190e8399e
aravindbhat <aravindcbk@gmail.com>
2025-06-21 17:33:45 +0530

    Testing commit

--- Solidity/SimpleStorage.sol
+++ Solidity/SimpleStorage.sol
@@ -2,5 +2,9 @@
 pragma solidity 0.8.19;
 
 contract SimpleStorage{
-    
+    bool hasFavouriteNumber=true;
+    uint256 favouriteNumber =88;
+    string favouriteNumberText="eighty-eight";
+    int256 favouriteInt=-88;
+     
 }
